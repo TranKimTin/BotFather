d338b1241b09d2d86f5d8c5d66078ed554390f69
Trần Kim Tín <tintk62@wru.vn>
2023-11-22 18:20:00 +0700

    Revert "Update util.ts"
    This reverts commit e9f7b1c635e441cdab8508ffee69a5e49787a77d.

--- src/util.ts
+++ src/util.ts
@@ -265,7 +265,8 @@ let cache = {
 async function getOHLCV_m1(symbol: string, limit: number, since: number): Promise<Array<OHLCV>> {
 
     if (cache.key == `${symbol}_${limit}_${since}`
-        && cache.lastTime + 30000 > new Date().getTime()) {
+        && cache.lastTime + 10000 > new Date().getTime()
+        && cache.lastData[cache.lastData.length - 1]?.startTime == getStartTime('1m', new Date().getTime())) {
         return cache.lastData;
     }
 
