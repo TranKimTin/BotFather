847ea17b258483cbf1d1151ff5cb0dfb3acdbd3a
Trần Kim Tín <tintk62@wru.vn>
2023-11-27 10:42:25 +0700

    change port 80->83

--- src/bot80.ts
+++
@@ -1,4 +0,0 @@
-import BotRSI_CCI from "./bot_rsi_cci";
-
-let bot = new BotRSI_CCI(1402460613, 1995047677, 253910119, 'config80.txt', 80, 'BOT_80', true);
-bot.init(0);
---
+++ src/bot83.ts
@@ -0,0 +1,4 @@
+import BotRSI_CCI from "./bot_rsi_cci";
+
+let bot = new BotRSI_CCI(1402460613, 1995047677, 253910119, 'config83.txt', 83, 'BOT_83', true);
+bot.init(0);
