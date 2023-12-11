1ee1c828cdc7180132bb6e29775915f32cafa7e5
Trần Kim Tín <tintk62@wru.vn>
2023-12-11 09:18:18 +0700

    a

--- src/bot81.ts
+++ src/bot81.ts
@@ -1,4 +1,4 @@
 import BotRSI_CCI from "./bot_rsi_cci";
 
-let bot = new BotRSI_CCI(666168084, 162876164, 1758247327, 'config81.txt', 81, 'BOT_81', false);
+let bot = new BotRSI_CCI(666168084, 162876164, 1758247327, 'config81.txt', 81, 'BOT_81', true);
 bot.init(3 * 60 * 1000);
