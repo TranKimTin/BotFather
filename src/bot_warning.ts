5466a0bff6d0bc41426290740c7fb34dd42d0b0f
Trần Kim Tín <kimtin.tr@gmail.com>
2024-03-25 14:37:05 +0700

    Create bot_warning.ts

---
+++ src/bot_warning.ts
@@ -0,0 +1,44 @@
+import BinanceFuture, { RateData } from './BinanceFuture';
+import Telegram from './telegram';
+import * as util from './util';
+
+
+let telegram = new Telegram("");
+
+async function main() {
+    console.log('Bot warning');
+    let binance = new BinanceFuture({
+        apiKey: process.env.API_KEY,
+        secretKey: process.env.SECRET_KEY,
+        symbolList: ['BTCUSDT', 'LTCUSDT', 'ALTUSDT', 'JUPUSDT', 'ETHUSDT', 'AEVOUSDT'],
+        timeframes: ['15m'],
+        onCloseCandle: onCloseCandle,
+        onClosePosition: async (symbol: string) => { },
+        onHandleError: async (err: any, symbol: string | undefined) => { await telegram.sendError((symbol ? symbol : '') + err.message); },
+        onInitStart: async () => { await telegram.sendMessage('bot restart...') },
+        onInitDone: async () => { await telegram.sendMessage('bot restart done.') },
+        isReadOnly: true
+    });
+    await binance.init();
+}
+
+async function onCloseCandle(symbol: string, timeframe: string, data: Array<RateData>) {
+    let RSI = util.iRSI(data, 14);
+    if (RSI.length == 0) return;
+
+    let rsi = +RSI[0].toFixed(2);
+    console.log({ symbol, timeframe, rsi });
+    if (rsi >= 70 || rsi <= 30) {
+        let dataTable = [
+            ['Timestamp', data[0].timestring],
+            ['Symbol', symbol],
+            ['Price', data[0].close],
+            ['RSI', rsi],
+            ['Symbol', symbol],
+        ];
+        await telegram.sendTable(dataTable);
+    }
+}
+
+
+main();
