66c05f512eb1fc67091ceac28337b37b84400620
tin.tran <tin.tran@lge.com>
2024-06-20 18:03:50 +0700

    a

--- src/util.ts
+++ src/util.ts
@@ -400,7 +400,7 @@ export function iMACD(data: Array<RateData>, fastEMA: number, slowEMA: number, s
         });
     }
 
-    return result;
+    return result.reverse();
 }
 
 function getEMA(prices: Array<number>, period: number) {
