1bd1da69a37e2ae41315d4da1c589c229626b82e
Trần Kim Tín <kimtin.tr@gmail.com>
2024-09-05 00:10:18 +0700

    Update botFather.ts

--- src/botFather.ts
+++ src/botFather.ts
@@ -475,9 +475,9 @@ export class BotFather {
                                     i++;
                                 }
                             }
-                            console.log({ enableDivergence, preIndexMaxMACD, indexMaxMACD, m1: values[preIndexMaxMACD], m2: values[indexMaxMACD], indexMaxPrice, preIndexMaxPrice, p: data[preIndexMaxPrice], p2: data[indexMaxPrice], diff: diffPercents[0] });
+                            // console.log({ enableDivergence, preIndexMaxMACD, indexMaxMACD, m1: values[preIndexMaxMACD], m2: values[indexMaxMACD], indexMaxPrice, preIndexMaxPrice, p: data[preIndexMaxPrice], p2: data[indexMaxPrice], diff: diffPercents[0] });
 
-                            if (enableDivergence === 1 && values[preIndexMaxMACD] <= values[indexMaxMACD]) {
+                            if (enableDivergence === 1 && values[preIndexMaxMACD].MACD <= values[indexMaxMACD].MACD) {
                                 value = 0;
                                 break;
                             }
