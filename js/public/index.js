ad05b2356dfe2b1a40cbb6587241c230f403c98e
Trần Kim Tín <kimtin.tr@gmail.com>
2024-09-29 00:18:15 +0700

    Update index.js

--- js/public/index.js
+++ js/public/index.js
@@ -368,7 +368,7 @@ $(document).ready(function () {
         });
     }
 
-    $('#botName').on('keydown click', function (event) {
+    $('#botName').on('keydown change', function (event) {
         clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
             let botName = $('#botName').val();
