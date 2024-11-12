213feb3759096974f4231ca7ace01570339fb66c
Trần Kim Tín <kimtin.tr@gmail.com>
2024-11-13 02:57:13 +0700

    Update UpdateOrderStatus.ts

--- src/UpdateOrderStatus.ts
+++ src/UpdateOrderStatus.ts
@@ -37,129 +37,140 @@ const ports: { [key: string]: number } = {
 };
 
 async function handleOrder(order: Order) {
-    const { broker, symbol, lastTimeUpdated } = order;
+    try {
+        const { broker, symbol, lastTimeUpdated } = order;
 
-    const url = `${hostSocketServer}:${ports[broker]}/api/getOHLCV`;
-    const params = {
-        symbol,
-        timeframe: '1m',
-        since: lastTimeUpdated ? (lastTimeUpdated + 60000) : (order.createdTime - 60000)
-    };
-    const data: Array<RateData> = await axios.get(url, { params })
-        .then(res => res.data.reverse()); //time tang dan
+        const url = `${hostSocketServer}:${ports[broker]}/api/getOHLCV`;
+        const params = {
+            symbol,
+            timeframe: '1m',
+            since: lastTimeUpdated ? (lastTimeUpdated + 60000) : (order.createdTime - 60000)
+        };
+        const data: Array<RateData> = await axios.get(url, { params })
+            .then(res => res.data.reverse()); //time tang dan
 
-    console.log(order.id, order.symbol, order.broker, order.timeframe, order.status, data.length)
-    for (let rate of data) {
-        if (!rate.isFinal) break;
-        if ([ORDER_STATUS.CANCELED, ORDER_STATUS.MATCH_TP, ORDER_STATUS.MATCH_SL].includes(order.status)) break;
+        console.log(order.id, order.symbol, order.broker, order.timeframe, order.status, data.length)
+        for (let rate of data) {
+            if (!rate.isFinal) break;
+            if ([ORDER_STATUS.CANCELED, ORDER_STATUS.MATCH_TP, ORDER_STATUS.MATCH_SL].includes(order.status)) break;
 
-        if (order.status === ORDER_STATUS.OPENED
-            && [NODE_TYPE.BUY_STOP_MARKET, NODE_TYPE.BUY_STOP_LIMIT].includes(order.orderType)) {
-            //handle stop buy
-            if (rate.high >= order.stop) {
-                order.status = ORDER_STATUS.MATCH_STOP;
-                order.timeStop = rate.startTime;
+            if (order.status === ORDER_STATUS.OPENED
+                && [NODE_TYPE.BUY_STOP_MARKET, NODE_TYPE.BUY_STOP_LIMIT].includes(order.orderType)) {
+                //handle stop buy
+                if (rate.high >= order.stop) {
+                    order.status = ORDER_STATUS.MATCH_STOP;
+                    order.timeStop = rate.startTime;
+                }
             }
-        }
-        if (order.status === ORDER_STATUS.OPENED
-            && [NODE_TYPE.SELL_STOP_MARKET, NODE_TYPE.SELL_STOP_LIMIT].includes(order.orderType)) {
-            //handle stop sell
-            if (rate.low <= order.stop) {
-                order.status = ORDER_STATUS.MATCH_STOP;
-                order.timeStop = rate.startTime;
+            if (order.status === ORDER_STATUS.OPENED
+                && [NODE_TYPE.SELL_STOP_MARKET, NODE_TYPE.SELL_STOP_LIMIT].includes(order.orderType)) {
+                //handle stop sell
+                if (rate.low <= order.stop) {
+                    order.status = ORDER_STATUS.MATCH_STOP;
+                    order.timeStop = rate.startTime;
+                }
             }
-        }
 
-        if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.BUY_STOP_LIMIT)
-            || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.BUY_LIMIT)) {
-            //handle entry buy limit
-            if (rate.low <= order.entry) {
+            if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.BUY_STOP_LIMIT)
+                || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.BUY_LIMIT)) {
+                //handle entry buy limit
+                if (rate.low <= order.entry) {
+                    order.status = ORDER_STATUS.MATCH_ENTRY;
+                    order.timeEntry = rate.startTime;
+                }
+            }
+            if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.SELL_STOP_LIMIT)
+                || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.SELL_LIMIT)) {
+                //handle entry sell limit
+                if (rate.high >= order.entry) {
+                    order.status = ORDER_STATUS.MATCH_ENTRY;
+                    order.timeEntry = rate.startTime;
+                }
+            }
+
+            if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.BUY_STOP_MARKET)
+                || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.BUY_MARKET)) {
+                //handle entry buy market
                 order.status = ORDER_STATUS.MATCH_ENTRY;
                 order.timeEntry = rate.startTime;
             }
-        }
-        if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.SELL_STOP_LIMIT)
-            || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.SELL_LIMIT)) {
-            //handle entry sell limit
-            if (rate.high >= order.entry) {
+            if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.SELL_STOP_MARKET)
+                || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.SELL_MARKET)) {
+                //handle entry sell market
                 order.status = ORDER_STATUS.MATCH_ENTRY;
                 order.timeEntry = rate.startTime;
             }
-        }
 
-        if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.BUY_STOP_MARKET)
-            || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.BUY_MARKET)) {
-            //handle entry buy market
-            order.status = ORDER_STATUS.MATCH_ENTRY;
-            order.timeEntry = rate.startTime;
-        }
-        if ((order.status === ORDER_STATUS.MATCH_STOP && order.orderType === NODE_TYPE.SELL_STOP_MARKET)
-            || (order.status === ORDER_STATUS.OPENED && order.orderType === NODE_TYPE.SELL_MARKET)) {
-            //handle entry sell market
-            order.status = ORDER_STATUS.MATCH_ENTRY;
-            order.timeEntry = rate.startTime;
-        }
+            if (order.status === ORDER_STATUS.MATCH_ENTRY
+                && [NODE_TYPE.BUY_MARKET, NODE_TYPE.BUY_LIMIT, NODE_TYPE.BUY_STOP_MARKET, NODE_TYPE.BUY_STOP_LIMIT].includes(order.orderType)) {
+                //handle TP SL BUY
+                if (rate.low <= order.sl) {
+                    order.status = ORDER_STATUS.MATCH_SL;
+                    order.timeSL = rate.startTime;
+                }
+                else if (rate.high >= order.tp) {
+                    order.status = ORDER_STATUS.MATCH_TP;
+                    order.timeTP = rate.startTime;
+                }
+            }
+            if (order.status === ORDER_STATUS.MATCH_ENTRY
+                && [NODE_TYPE.SELL_MARKET, NODE_TYPE.SELL_LIMIT, NODE_TYPE.SELL_STOP_MARKET, NODE_TYPE.SELL_STOP_LIMIT].includes(order.orderType)) {
+                //handle TP SL SELL
+                if (rate.high >= order.sl) {
+                    order.status = ORDER_STATUS.MATCH_SL;
+                    order.timeSL = rate.startTime;
+                }
+                else if (rate.low <= order.tp) {
+                    order.status = ORDER_STATUS.MATCH_TP;
+                    order.timeTP = rate.startTime;
+                }
+            }
 
-        if (order.status === ORDER_STATUS.MATCH_ENTRY
-            && [NODE_TYPE.BUY_MARKET, NODE_TYPE.BUY_LIMIT, NODE_TYPE.BUY_STOP_MARKET, NODE_TYPE.BUY_STOP_LIMIT].includes(order.orderType)) {
-            //handle TP SL BUY
-            if (rate.low <= order.sl) {
-                order.status = ORDER_STATUS.MATCH_SL;
-                order.timeSL = rate.startTime;
+            if (order.status === ORDER_STATUS.OPENED && order.expiredTime) {
+                //handle expired time
+                if (order.expiredTime >= rate.startTime) {
+                    order.status = ORDER_STATUS.CANCELED;
+                }
             }
-            else if (rate.high >= order.tp) {
-                order.status = ORDER_STATUS.MATCH_TP;
-                order.timeTP = rate.startTime;
-            }
-        }
-        if (order.status === ORDER_STATUS.MATCH_ENTRY
-            && [NODE_TYPE.SELL_MARKET, NODE_TYPE.SELL_LIMIT, NODE_TYPE.SELL_STOP_MARKET, NODE_TYPE.SELL_STOP_LIMIT].includes(order.orderType)) {
-            //handle TP SL SELL
-            if (rate.high >= order.sl) {
-                order.status = ORDER_STATUS.MATCH_SL;
-                order.timeSL = rate.startTime;
-            }
-            else if (rate.low <= order.tp) {
-                order.status = ORDER_STATUS.MATCH_TP;
-                order.timeTP = rate.startTime;
-            }
-        }
 
-        if (order.status === ORDER_STATUS.OPENED && order.expiredTime) {
-            //handle expired time
-            if (order.expiredTime >= rate.startTime) {
-                order.status = ORDER_STATUS.CANCELED;
-            }
+            order.lastTimeUpdated = rate.startTime;
         }
-
-        order.lastTimeUpdated = rate.startTime;
+        const sql = `UPDATE Orders 
+                        SET status = ?, timeStop = ?, timeEntry=?, timeTP = ?, timeSL = ?, lastTimeUpdated = ?
+                        WHERE id = ?`;
+        const args = [order.status, order.timeStop, order.timeEntry, order.timeTP, order.timeSL, order.lastTimeUpdated, order.id];
+        await mysql.query(sql, args);
     }
-    const sql = `UPDATE Orders 
-                    SET status = ?, timeStop = ?, timeEntry=?, timeTP = ?, timeSL = ?, lastTimeUpdated = ?
-                    WHERE id = ?`;
-    const args = [order.status, order.timeStop, order.timeEntry, order.timeTP, order.timeSL, order.lastTimeUpdated, order.id];
-    await mysql.query(sql, args);
+    catch (err) {
+        console.log(err);
+    }
 }
 
 async function main() {
-    const orders: Array<Order> = await mysql.query(
-        `SELECT * FROM Orders WHERE status NOT IN (?, ?, ?)`,
-        [ORDER_STATUS.MATCH_TP, ORDER_STATUS.MATCH_SL, ORDER_STATUS.CANCELED]
-    );
-    console.log(`update ${orders.length} order`);
+    try {
+        const orders: Array<Order> = await mysql.query(
+            `SELECT * FROM Orders WHERE status NOT IN (?, ?, ?)`,
+            [ORDER_STATUS.MATCH_TP, ORDER_STATUS.MATCH_SL, ORDER_STATUS.CANCELED]
+        );
+        console.log(`update ${orders.length} order`);
 
-    let promiseList = [];
+        let promiseList = [];
 
-    for (const order of orders) {
-        promiseList.push(handleOrder(order));
-        if (promiseList.length > 10) {
-            await Promise.all(promiseList);
-            promiseList = [];
+        for (const order of orders) {
+            promiseList.push(handleOrder(order));
+            if (promiseList.length > 10) {
+                await Promise.all(promiseList);
+                promiseList = [];
+            }
         }
+        await Promise.all(promiseList);
     }
-    await Promise.all(promiseList);
-
-    setTimeout(main, 5 * 60 * 1000);
+    catch (err) {
+        console.error(err);
+    }
+    finally {
+        setTimeout(main, 5 * 60 * 1000);
+    }
 }
 
 main();
