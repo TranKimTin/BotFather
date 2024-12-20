ab69e67dff63d8290b69c1de1eb3e07c8fed3fd0
tin.tran <tin.tran@lge.com>
2024-12-20 11:53:40 +0700

    fix build error

---
+++ front-end/src/App.ts
@@ -0,0 +1,32 @@
+import { ref } from 'vue';
+import Toast from 'primevue/toast';
+import ConfirmDialog from 'primevue/confirmdialog';
+import * as mToast from './toast/toast';
+
+export default {
+  components: { Toast, ConfirmDialog },
+  setup() {
+    mToast.showInfo("Xin chào");
+
+    const sidebarVisible = ref(true);
+    const activeItem = ref('Home');
+
+    const menuItems = [
+      { label: 'Trang chủ', route: '/', icon: 'pi pi-home' },
+      { label: 'Lịch sử lệnh', route: '/history', icon: 'pi pi-book' },
+      { label: 'Máy tính', route: '/calculator', icon: 'pi pi-calculator' },
+      { label: 'Thanh toán', route: '/contact', icon: 'pi pi-credit-card' },
+      { label: 'Cài đặt', route: '/team', icon: 'pi pi-cog' },
+    ];
+
+    const toggleSidebar = () => {
+      sidebarVisible.value = !sidebarVisible.value;
+    };
+
+    const setActive = (item: string) => {
+      activeItem.value = item;
+    };
+
+    return { sidebarVisible, menuItems, activeItem, toggleSidebar, setActive };
+  },
+};
--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -34,40 +34,7 @@
   </div>
 </template>
 
-<script>
-import { ref } from 'vue';
-import Toast from 'primevue/toast';
-import ConfirmDialog from 'primevue/confirmdialog';
-import * as mToast from './toast/toast';
-
-export default {
-  components: { Toast, ConfirmDialog },
-  setup() {
-    mToast.showInfo("Xin chào");
-
-    const sidebarVisible = ref(true);
-    const activeItem = ref('Home');
-
-    const menuItems = [
-      { label: 'Trang chủ', route: '/', icon: 'pi pi-home' },
-      { label: 'Lịch sử lệnh', route: '/history', icon: 'pi pi-book' },
-      { label: 'Máy tính', route: '/calculator', icon: 'pi pi-calculator' },
-      { label: 'Thanh toán', route: '/contact', icon: 'pi pi-credit-card' },
-      { label: 'Cài đặt', route: '/team', icon: 'pi pi-cog' },
-    ];
-
-    const toggleSidebar = () => {
-      sidebarVisible.value = !sidebarVisible.value;
-    };
-
-    const setActive = (item) => {
-      activeItem.value = item;
-    };
-
-    return { sidebarVisible, menuItems, activeItem, toggleSidebar, setActive };
-  },
-};
-</script>
+<script lang="ts" src="./App.ts"></script>
 
 <style scoped>
 /* Layout */
