fcee4986d76537c66ad54bac42037f2d5f0f826e
Trần Kim Tín <kimtin.tr@gmail.com>
2025-01-19 22:27:25 +0700

    update cache

--- src/common/Cache.ts
+++ src/common/Cache.ts
@@ -112,7 +112,7 @@ export function get(key: string) {
     update();
     cntAll++;
     if (data[key]?.value) cntCache++;
-    console.log(`cache ${cntCache} / ${cntAll} (${cntCache / cntAll * 100} %)`);
+    console.log(`cache size: ${heap.size()} - ${cntCache} / ${cntAll} (${cntCache / cntAll * 100} %)`);
     return data[key]?.value || null;
 }
 
