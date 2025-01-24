daebbae337500b8b0ecd0135f6d3c1a427927396
tin.tran <tin.tran@lge.com>
2025-01-24 16:40:43 +0700

    fix bug

--- src/WebConfig/business/Dashboard.ts
+++ src/WebConfig/business/Dashboard.ts
@@ -10,7 +10,7 @@ export async function getBotInfo(userData: UserTokenInfo) {
                     COUNT(IF(o.timeSL IS NULL AND o.timeTP IS NULL, 1, NULL)) tradeCountOpening,
                     SUM(IF(o.timeSL IS NOT NULL OR o.timeTP IS NOT NULL, o.profit, 0)) AS profit,
                     SUM(IF(o.timeSL IS NULL AND o.timeTP IS NULL, o.profit, 0)) AS unrealizedProfit,
-                    SUM(IF(o.timeSL IS NOT NULL OR o.timeTP IS NOT NULL, o.entry * o.volume, 0)) AS volumeClosed,
+                    SUM(IF(o.timeSL IS NOT NULL OR o.timeTP IS NOT NULL, (o.entry + IF(o.timeSL IS NOT NULL, o.sl, o.tp)) / 2 * o.volume, 0)) AS volumeClosed,
                     SUM(IF(o.timeSL IS NULL AND o.timeTP IS NULL, o.entry * o.volume, 0)) AS volumeOpening,
                     COUNT(IF(o.profit >= 0 AND ( o.timeSL IS NOT NULL OR o.timeTP IS NOT NULL), 1, NULL)) / COUNT(1) * 100 AS winrate
                 FROM Bot b
