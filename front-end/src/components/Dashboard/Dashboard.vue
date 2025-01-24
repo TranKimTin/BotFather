ea1dc1021b0140fe4a82975237016aef629b4778
tin.tran <tin.tran@lge.com>
2025-01-24 16:23:35 +0700

    update UI

--- front-end/src/components/Dashboard/Dashboard.vue
+++ front-end/src/components/Dashboard/Dashboard.vue
@@ -108,7 +108,7 @@
                                 </th>
                                 <th
                                     class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                    Lãi / volume (chưa đóng)
+                                    Lãi / volume (đang mở)
                                 </th>
                                 <th
                                     class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
