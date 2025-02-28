c1199236e8b076b648ab63d90b4121a65c7db032
tin.tran <tin.tran@lge.com>
2025-02-28 12:47:02 +0700

    add log debug

--- src/worker_socket.ts
+++ src/worker_socket.ts
@@ -86,8 +86,9 @@ async function onCloseCandle(broker: string, symbol: string, timeframe: string, 
         // cacheIndicators[key] = result.cacheIndicator;
         // console.log(`onCloseCandle ${broker} ${symbol} ${timeframe} runtime = ${result.runtime} ms`);
         // console.log(result)
-        const runtime = worker.onCloseCandle(broker, symbol, timeframe, data, cacheIndicators[key])
+        const runtime = worker.onCloseCandle(broker, symbol, timeframe, data, cacheIndicators[key]);
         console.log(`onCloseCandle ${broker} ${symbol} ${timeframe} runtime = ${runtime} ms`);
+        console.log(cacheIndicators);
     }
     catch (err) {
         console.error('onCloseCandle error', err);
