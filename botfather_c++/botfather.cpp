c1bfc67ecfc08a1d5fcf59afb834cc256afc092c
Trần Kim Tín <kimtin.tr@gmail.com>
2025-04-18 13:46:58 +0700

    init

--- botfather_c++/botfather.cpp
+++ botfather_c++/botfather.cpp
@@ -1,7 +1,23 @@
-#include<iostream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <vector>
 using namespace std;
-int main() {
-	cout << "Hello world";
 
-	return 0;
+int main()
+{
+
+    auto t1 = clock();
+
+    for (int k = 0; k < 10; k++)
+    {
+        vector<int> a;
+        long long x = 1;
+        for (int i = 0; i < 10000000; i++)
+        {
+            a.push_back(i);
+        }
+    }
+    auto t2 = clock();
+    cout << "time=" << (t2 - t1) / 1000 << endl;
+    return 0;
 }
