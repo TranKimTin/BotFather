fe0fbf274ff1f314359a3715a48fbcc35003a752
Trần Kim Tín <kimtin.tr@gmail.com>
2025-07-12 18:11:04 +0700

    update lib

--- botfather_c++/lib/install.sh
+++ botfather_c++/lib/install.sh
@@ -1,16 +1,13 @@
 set -e
 
-rm -rf antlr4_repo
-rm -rf antlr4
-git clone https://github.com/antlr/antlr4.git antlr4_repo
+if [ ! -d "antlr4" ]; then
+  git clone https://github.com/antlr/antlr4.git antlr4
+fi
 
-cmake -S antlr4_repo/runtime/Cpp -B antlr4_repo/runtime/Cpp/build \
-  -DCMAKE_INSTALL_PREFIX="$(pwd)/antlr4" \
+cmake -S antlr4/runtime/Cpp -B antlr4/runtime/Cpp/build \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -march=native -flto -funroll-loops -DNDEBUG" \
   -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-flto"
 
-cmake --build antlr4_repo/runtime/Cpp/build --parallel $(nproc)
-cmake --install antlr4_repo/runtime/Cpp/build
-
-rm -rf antlr4_repo
+cmake --build antlr4/runtime/Cpp/build --parallel $(nproc)
+cmake --install antlr4/runtime/Cpp/build
