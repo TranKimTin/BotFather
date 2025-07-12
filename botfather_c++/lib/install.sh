cf276a8da4e7955d6865f217b37fba22dd5f41f4
Trần Kim Tín <kimtin.tr@gmail.com>
2025-07-12 23:42:10 +0700

    Update install.sh

--- botfather_c++/lib/install.sh
+++ botfather_c++/lib/install.sh
@@ -30,7 +30,7 @@ fi
 echo "Building socket.io-client-cpp..."
 cmake -S socket.io-client-cpp -B socket.io-client-cpp/build \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_STANDARD=17
+  -DCMAKE_CXX_STANDARD=17 \
   -DCMAKE_CXX_FLAGS_RELEASE="-O3 -march=native -flto -funroll-loops -DNDEBUG" \
   -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-flto"
 
