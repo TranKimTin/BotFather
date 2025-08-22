dae82eed1cfa35d651936df6b955dbfadc24c8a3
Trần Kim Tín <kimtin.tr@gmail.com>
2025-08-22 14:59:13 +0700

    Update botfather.cpp

--- botfather_c++/src/botfather.cpp
+++ botfather_c++/src/botfather.cpp
@@ -232,8 +232,8 @@ void runApp()
 
 #ifndef TEST
 
-    exchanges.push_back(new SocketBinance(20));
-    exchanges.push_back(new SocketBinanceFuture(20));
+    exchanges.push_back(new SocketBinance(5));
+    exchanges.push_back(new SocketBinanceFuture(5));
     exchanges.push_back(new SocketBybit(5));
     exchanges.push_back(new SocketBybitFuture(5));
     exchanges.push_back(new SocketOkx(5));
