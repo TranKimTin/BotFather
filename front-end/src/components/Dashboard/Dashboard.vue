93f4e8352a25ba810d272d5af69c19dd8dc1dc19
Trần Kim Tín <kimtin.tr@gmail.com>
2025-08-27 01:09:54 +0700

    Update Dashboard.vue

--- front-end/src/components/Dashboard/Dashboard.vue
+++ front-end/src/components/Dashboard/Dashboard.vue
@@ -80,10 +80,10 @@
         <div class="mt-8" />
 
         <div class="flex flex-col mt-8">
-            <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div class="py-2 -my-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
                 <div
-                    class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-                    <table class="min-w-full">
+                    class="inline-block overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+                    <table>
                         <thead>
                             <tr>
                                 <th
