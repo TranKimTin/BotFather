ea696dcb9caec8a6dfd477dbbee3f8831f807ca3
Trần Kim Tín <kimtin.tr@gmail.com>
2025-08-31 23:48:50 +0700

    Update HistoryOrder.vue

--- front-end/src/components/HistoryOrder/HistoryOrder.vue
+++ front-end/src/components/HistoryOrder/HistoryOrder.vue
@@ -72,7 +72,8 @@
     <BalanceChart :data="r_balanceData" />
     <div>
         <h3 v-if="r_isLoading">Đang load, đợi tí...</h3>
-        <DataTable :value="r_orderList" tableStyle="min-width: 50rem" scrollable scrollHeight="90vh" stripedRows>
+        <DataTable :value="r_orderList" tableStyle="min-width: 50rem" scrollable scrollHeight="90vh"
+            scrollable:rows="100" stripedRows>
             <Column :header="`STT (${r_orderList.length})`">
                 <template #body="order">
                     {{ order.index + 1 }}
