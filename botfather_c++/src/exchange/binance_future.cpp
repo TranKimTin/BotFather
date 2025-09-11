5136a44da399ea4c8f37c088df9077e45b1ef694
Trần Kim Tín <kimtin.tr@gmail.com>
2025-09-12 02:15:50 +0700

    Update binance_future.cpp

--- botfather_c++/src/exchange/binance_future.cpp
+++ botfather_c++/src/exchange/binance_future.cpp
@@ -662,7 +662,6 @@ string BinanceFuture::getOrderStatus(const string &symbol, const string &orderId
 
     try
     {
-        LOGI("Get Order status with key: {}", apiKey);
         string res = Axios::get(url, {"X-MBX-APIKEY: " + apiKey});
         return res;
     }
