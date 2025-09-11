99db173140856673857453ef7040593f8fe0a49e
Trần Kim Tín <kimtin.tr@gmail.com>
2025-09-12 01:59:10 +0700

    Reapply "Merge pull request #38 from TranKimTin/BF-91-tp-sl-not-cancel-when-user-close-position-manual"
    This reverts commit b41ce7fcb4fc12c98f455ec0186180c562f27c21.

--- botfather_c++/src/botfather.cpp
+++ botfather_c++/src/botfather.cpp
@@ -47,7 +47,7 @@ void test()
     vector<long long> startTime(rateData.startTime.begin(), rateData.startTime.end());
 
     string iv = generateRandomIV();
-    string apiKey = encryptAES(env["API_KEY"], env["ENCRYP_KEY"], iv);
+    string apiKey = env["API_KEY"];
     string secretKey = encryptAES(env["SECRET_KEY"], env["ENCRYP_KEY"], iv);
     IExchange *exchange = new BinanceFuture(apiKey, secretKey, iv, 31);
 
