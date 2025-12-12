705d4aa0aa72d371cd665595cb2a420a00eca132
Trần Kim Tín <kimtin.tr@gmail.com>
2025-12-12 18:35:16 +0700

    Update util.cpp

--- botfather_c++/src/common/util.cpp
+++ botfather_c++/src/common/util.cpp
@@ -534,8 +534,8 @@ vector<string> getBinanceFutureSymbolList()
         if (status != "TRADING" || !endsWith(symbol, "USDT") || symbol == "USDCUSDT" || symbol == "TUSDUSDT" || symbol == "DAIUSDT")
             continue;
         symbols.push_back(symbol);
-        if (symbols.size() >= 500)
-            break;
+        // if (symbols.size() >= 500)
+            // break;
     }
     return symbols;
 }
