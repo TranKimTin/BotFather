31610007d997ea84a107ed74745a9ddc688e62a4
Trần Kim Tín <kimtin.tr@gmail.com>
2025-12-16 22:28:14 +0700

    update UI

--- front-end/src/components/Setting/Setting.ts
+++ front-end/src/components/Setting/Setting.ts
@@ -1,14 +1,30 @@
 import { defineComponent, ref, onMounted } from 'vue';
-// import * as axios from '../../axios/axios';
+import * as axios from '../../axios/axios';
 import * as Toast from '../../toast/toast';
+import Dialog from 'primevue/dialog';
+import Select from 'primevue/select';
+import InputNumber from 'primevue/inputnumber';
+import Button from 'primevue/button';
 
 export default defineComponent({
-    components: {},
+    components: { Dialog, Select, InputNumber, Button },
     setup() {
-        const testValue = ref<string>('');
+        const r_visible = ref<boolean>(false);
+        const r_botList = ref<Array<string>>([]);
+        const r_botName = ref<string>('');
+        const r_leverage = ref<number>(1);
+
+        function setLeverage() {
+            Toast.showSuccess(`Setting leverage ${r_botName.value} ${r_leverage.value}`);
+            r_visible.value = false;
+        }
         onMounted(() => {
+            axios.get('/getBotList', { real: 1 }).then(result => {
+                console.log(result);
+                r_botList.value = result;
+            });
         });
 
-        return {testValue};
+        return { r_visible, r_botList, r_botName, r_leverage, setLeverage };
     },
 });
