17acdd1cb5dc20ca2ba656357d58befd03279321
Trần Kim Tín (Trust) <kimtin.tr@gmail.com>
2026-01-06 15:34:00 +0700

    Update Backtest.vue

--- front-end/src/components/Backtest/Backtest.vue
+++ front-end/src/components/Backtest/Backtest.vue
@@ -88,10 +88,12 @@
           </Column>
           <Column field="profit" header="Lãi">
             <template #body="order">
-              <span v-if="order.data.matchTime" class="inline-flex px-2 text-xs font-semibold leading-5 rounded-full"
-                :class="order.data.profit >= 0 ? 'text-green-800 bg-green-100' : 'text-red-800 bg-red-100'"><span v-if="order.data.profit >= 0">+</span>{{
-                  (parseFloat(order.data.profit.toFixed(2))).toLocaleString() }} $ </span> (<span v-if="order.data.profit >= 0">+</span>{{ +(order.data.profit / (order.data.entry * order.data.volume) * 100).toFixed(2) }} %)
-            </template>
+              <span v-if="order.data.matchTime">
+                <span class="inline-flex px-2 text-xs font-semibold leading-5 rounded-full"
+                  :class="order.data.profit >= 0 ? 'text-green-800 bg-green-100' : 'text-red-800 bg-red-100'"><span v-if="order.data.profit >= 0">+</span>{{
+                    (parseFloat(order.data.profit.toFixed(2))).toLocaleString() }} $ </span> (<span v-if="order.data.profit >= 0">+</span>{{ +(order.data.profit / (order.data.entry * order.data.volume) * 100).toFixed(2) }} %)
+              </span>
+          </template>
           </Column>
           <Column field="status" header="Trạng thái"></Column>
         </DataTable>
