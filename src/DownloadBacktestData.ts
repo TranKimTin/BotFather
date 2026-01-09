9fd0f19b50c1d52114285012b1a2d5d91e010ab6
Trần Kim Tín (Trust) <kimtin.tr@gmail.com>
2026-01-09 10:02:20 +0700

    Update DownloadBacktestData.ts

--- src/DownloadBacktestData.ts
+++ src/DownloadBacktestData.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: '../.env' });
 
 async function main() {
     const symbolList = await util.getBinanceFutureSymbolList();
-    const since = new Date('2021-10-01').getTime();
+    const since = new Date('2019-10-01').getTime();
     let i = 0;
     for (const symbol of symbolList) {
         const months = (await util.getBinanceFutureOHLCV(symbol, '1M', 300, since)).map(item => moment(item.startTime).format('YYYY-MM')).reverse();
