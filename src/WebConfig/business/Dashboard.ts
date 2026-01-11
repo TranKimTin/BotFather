b0b697892b71c16fec37cd8262dec77ed755f412
Trần Kim Tín (Trust) <kimtin.tr@gmail.com>
2026-01-11 23:53:51 +0700

    BF-118 optimize dashboard

--- src/WebConfig/business/Dashboard.ts
+++ src/WebConfig/business/Dashboard.ts
@@ -12,37 +12,56 @@ export async function getBotInfo(userData: UserTokenInfo) {
 
     const accountInfo: { [key: string]: any } = {};
     const openOrders: { [key: string]: any[] } = {};
+    let promiseList: Array<Promise<any>> = [];
 
     for (const item of data) {
         if (item.apiKey && item.secretKey && item.iv) {
             const apiKey = item.apiKey;
             if (!accountInfo[apiKey]) {
-                const secretKey = util.decryptAES(item.secretKey, process.env.ENCRYP_KEY!, item.iv);
-                let client = Binance({
-                    apiKey: apiKey,
-                    apiSecret: secretKey
-                });
-                try {
-                    accountInfo[apiKey] = await client.futuresAccountInfo();
-                    accountInfo[apiKey].positions = accountInfo[apiKey].positions.filter((item: any) => item.initialMargin != '0');
-                    accountInfo[apiKey].positions.sort((a: any, b: any) => (+a.unrealizedProfit) - (+b.unrealizedProfit));
-                    openOrders[apiKey] = await client.futuresOpenOrders({});
-                    const algoOpenOrder = await client.futuresGetOpenAlgoOrders({}) as any;
-                    openOrders[apiKey].push(...algoOpenOrder);
+                accountInfo[apiKey] = {
+                    totalWalletBalance: 0,
+                    availableBalance: 0,
+                    totalUnrealizedProfit: 0
+                };
+                openOrders[apiKey] = [];
 
-                    openOrders[apiKey].sort((a: any, b: any) => a.symbol.localeCompare(b.symbol));
-                }
-                catch (err) {
-                    console.error(item.botName, err);
-                    accountInfo[apiKey] = {
-                        totalWalletBalance: 0,
-                        availableBalance: 0,
-                        totalUnrealizedProfit: 0
-                    };
-                    openOrders[apiKey] = [];
-                }
+                promiseList.push((async () => {
+                    const secretKey = util.decryptAES(item.secretKey, process.env.ENCRYP_KEY!, item.iv);
+                    let client = Binance({
+                        apiKey: apiKey,
+                        apiSecret: secretKey
+                    });
+                    try {
+                        accountInfo[apiKey] = await client.futuresAccountInfo();
+                        accountInfo[apiKey].positions = accountInfo[apiKey].positions.filter((item: any) => item.initialMargin != '0');
+                        accountInfo[apiKey].positions.sort((a: any, b: any) => (+a.unrealizedProfit) - (+b.unrealizedProfit));
+                        openOrders[apiKey] = await client.futuresOpenOrders({});
+                        const algoOpenOrder = await client.futuresGetOpenAlgoOrders({}) as any;
+                        openOrders[apiKey].push(...algoOpenOrder);
 
+                        openOrders[apiKey].sort((a: any, b: any) => a.symbol.localeCompare(b.symbol));
+                    }
+                    catch (err) {
+                        console.error(item.botName, err);
+                    }
+                })());
+
+                if (promiseList.length >= 10) {
+                    await Promise.all(promiseList);
+                    promiseList = [];
+                }
             }
+            item.accountInfo = accountInfo[apiKey];
+            item.openOrders = openOrders[apiKey];
+        }
+    }
+    if (promiseList.length >= 10) {
+        await Promise.all(promiseList);
+        promiseList = [];
+    }
+    for (const item of data) {
+        if (item.apiKey && item.secretKey && item.iv) {
+            const apiKey = item.apiKey;
             item.accountInfo = accountInfo[apiKey];
             item.openOrders = openOrders[apiKey];
         }
@@ -62,5 +81,6 @@ export async function getBotInfo(userData: UserTokenInfo) {
             item.winrate = 0;
         }
     }
+
     return data;
 }
