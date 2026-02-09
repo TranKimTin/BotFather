f9b2ec0511da7b4f7ece9d25dbb343851bd5bea3
Trần Kim Tín (Trust) <kimtin.tr@gmail.com>
2026-02-09 23:54:03 +0700

    update

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 const defaultLayout = 'default'
@@ -9,6 +9,13 @@ const { currentRoute } = useRouter()
 const layout = computed(
     () => `${currentRoute.value.meta.layout || defaultLayout}-layout`,
 )
+
+onMounted(() => {
+    document.addEventListener("keydown", function (d) {
+        (d.key === "F12") && console.log("%cÁi chà, Anh/Chị nhấn F12 tính làm gì đấy!", "color: red; font-weight: bold; font-size: 25px; border-style: dashed; padding: 10px;")
+    })
+})
+
 </script>
 
 <template>
